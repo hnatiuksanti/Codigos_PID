7e4534ef5137af715d86778483402c9ff939c530
PedritusMaximus <hnatiuksanti@gmail.com>
2022-08-24 01:32:01 -0300

    Se agrego un breve calculo control

--- PID_basico_Diego.py
+++ PID_basico_Diego.py
@@ -30,7 +30,7 @@ ser.flushInput()
 str = 'V0\n\r'
 ser.write(bytes(str,'utf-8'))
 time.sleep(0.002)
-s = ser.readline(25)
+s = ser.readline(25) #Lee 25 bytes y devuelve 25 bytes 
 print(s)
 
 #%% Defino funciones de comunicacion con el controlador
@@ -68,7 +68,7 @@ def voltajeCorregido(voltaje):
 resetControlador(ser)
 time.sleep(0.2)
 
-#inicializo variables
+#inicializo variables. Todas con el mismo len y todas vacias, por eso el "np.nan"
 voltajes = np.concatenate((0*np.ones(10) ,5*np.ones(50), 0*np.ones(140)))
 N = len(voltajes)
 posiciones = np.zeros(N)
@@ -99,3 +99,34 @@ axs[2].plot(tiempos, velocidades,'.-')
 axs[2].set(ylabel = 'Velocidad')
 plt.legend(('Medida por la PC','Pedida por la placa'))
 plt.xlabel('Tiempo [s]')
+
+#%%
+
+setpoint_pos = 0.0; setpoint_vel = 0.0
+
+setpoint = [setpoint_pos, setpoint_vel]
+variable = [posiciones, velocidades]
+
+""" ------------------ Señal de control --------------- """
+
+T = 1 #Tiempo de muestreo.
+
+def señal_de_control(i,T,N):
+    k_p = 1.0
+    k_i = 1.0
+    k_d = 1.0
+    
+    integral = 0 # Valor inicial de la integral. 
+    error_0 = 0 #Error inicial.
+    
+    for j in range(N):
+        error = setpoint[j] - variable[i][j]   # variable[0] recorre las pos. variable[1] recorre las vel.
+        proporcional = k_p*error
+        integral = integral + k_i*error*T
+        derivativo = k_d*(error - error_0)/T
+        error_0 = error
+        u = proporcional + integral + derivativo
+    return u
+    
+    
+    
